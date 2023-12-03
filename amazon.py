0705b7e5451624227a834bc10314bd2beffca757
Hiromu1612 <150511546+Hiromu1612@users.noreply.github.com>
2023-12-03 10:03:25 +0900

    try追加

--- amazon.py
+++ amazon.py
@@ -26,42 +26,49 @@ driver = webdriver.Chrome(options=options)
 #暗黙的な待機
 driver.implicitly_wait(10)
 
-#*Amazonのurlを取得
+
 def amazon(word):
-    print("Amazon_スクレイピング開始")
-    driver.get("https://www.amazon.co.jp/")
+    
+    try:#*Amazonのurlを取得
+        print("Amazon_スクレイピング開始")
+        driver.get("https://www.amazon.co.jp/")
 
-    #暗黙的な待機
-    driver.implicitly_wait(10)
+        #暗黙的な待機
+        driver.implicitly_wait(10)
 
-    #検索欄のタグを取得、keywordと入力
-    text_box=driver.find_element_by_id("twotabsearchtextbox")
-    text_box.send_keys(word)
+        #検索欄のタグを取得、keywordと入力
+        text_box=driver.find_element_by_id("twotabsearchtextbox")
+        text_box.send_keys(word)
 
-    #検索ボタンのタグを取得、クリック
-    btn=driver.find_element_by_id("nav-search-submit-button")
-    btn.click()
+        #検索ボタンのタグを取得、クリック
+        btn=driver.find_element_by_id("nav-search-submit-button")
+        btn.click()
 
-    print("Amazon_検索完了")
+        print("Amazon_検索完了")
 
-    #検索結果一覧のurlを取得し、beautiful soupのrequestsで価格をまとめて取得
-    from bs4 import BeautifulSoup
+        #検索結果一覧のurlを取得し、beautiful soupのrequestsで価格をまとめて取得
+        from bs4 import BeautifulSoup
 
-    #検索結果のurlを取得し、きれいに抽出
-    page_source=driver.page_source
-    html_amazon=BeautifulSoup(page_source,"lxml")
+        #検索結果のurlを取得し、きれいに抽出
+        page_source=driver.page_source
+        html_amazon=BeautifulSoup(page_source,"lxml")
 
-    #商品名、価格、送料、ポイント,検索結果のurlを表示
-    product_name_amazon=html_amazon.find(class_="a-size-base-plus a-color-base a-text-normal")
-    price_amazon=html_amazon.find(class_="a-price-whole")
-    shipping_fee_amazon=html_amazon.find(class_="a-row a-size-base a-color-secondary s-align-children-center")
-    #ポイントだけ抽出、正規表現で数字だけ取り出す
-    point=html_amazon.find(class_="a-size-base a-color-price")
-    point_amazon=re.findall("[0-9]+",point.text)
-    url_amazon=driver.current_url
+        #商品名、価格、送料、ポイント,検索結果のurlを表示
+        product_name_amazon=html_amazon.find(class_="a-size-base-plus a-color-base a-text-normal")
+        price_amazon=html_amazon.find(class_="a-price-whole")
+        shipping_fee_amazon=html_amazon.find(class_="a-row a-size-base a-color-secondary s-align-children-center")
+        #ポイントだけ抽出、正規表現で数字だけ取り出す
+        point=html_amazon.find(class_="a-size-base a-color-price")
+        point_amazon=re.findall("[0-9]+",point.text)
+        url_amazon=driver.current_url
 
-    #リスト化、カンマを取り除いて見やすくする
-    list_amazon=["【Amazon】",product_name_amazon.text,"￥"+price_amazon.text,shipping_fee_amazon.contents[1].text,point_amazon[0],url_amazon]
+        #リスト化、カンマを取り除いて見やすくする
+        list_amazon=["【Amazon】",product_name_amazon.text,"￥"+price_amazon.text,shipping_fee_amazon.contents[1].text,point_amazon[0],url_amazon]
 
-    print("Amazon_スクレイピング完了")
+        print("Amazon_スクレイピング完了")
+
+    except:
+        print("Amazon_スクレイピング失敗")
+        list_amazon=["【Amazon】","-","-","-","-","-"]
+
     return list_amazon
--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template,request
 import concurrent.futures
-from flask_socketio import SocketIO, emit
+# from flask_socketio import SocketIO, emit
 import os
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@socketio.on('start_scraping')
-def handle_scraping(data):
-    # スクレイピング処理を開始
-    for i in range(100):
-        # スクレイピングの進行状況をクライアントに送信
-        emit('scraping_progress', {'message': "Amazon_スクレイピング完了"})
+# @socketio.on('start_scraping')
+# def handle_scraping(data):
+#     # スクレイピング処理を開始
+#     for i in range(100):
+#         # スクレイピングの進行状況をクライアントに送信
+#         emit('scraping_progress', {'message': "Amazon_スクレイピング完了"})
 
 
 @app.route('/scraping', methods=["GET","POST"])
@@ -25,7 +25,6 @@ def do_scraping():
     import rakutenn
     import PayPayhurima
     import kakakucom
-    import logging
     word=request.form["search_word"]
     with concurrent.futures.ThreadPoolExecutor() as executor:
         future_amazon = executor.submit(amazon.amazon, word)
@@ -36,22 +35,14 @@ def do_scraping():
     for future in concurrent.futures.as_completed([future_amazon, future_rakutenn, future_PayPayhurima, future_kakakucom]):
         if future == future_amazon:
             list_amazon = future.result()
-            logging.info(f'Amazon: {list_amazon}')  # ログ出力
         elif future == future_rakutenn:
             list_rakutenn = future.result()
-            logging.info(f'rakutenn: {list_rakutenn}')  # ログ出力
         elif future == future_PayPayhurima:
             list_PayPayhurima = future.result()
-            logging.info(f'PayPay: {list_PayPayhurima}')  # ログ出力
         elif future == future_kakakucom:
             list_kakakucom = future.result()
-            logging.info(f'kakakucom: {list_kakakucom}')  # ログ出力
 
-    # ログファイルの内容を読み取る
-    with open('app.log', 'r') as f:
-        logs = f.read()
-
-    return render_template('result.html',word=word,list_amazon=list_amazon,list_rakutenn=list_rakutenn,list_PayPayhurima=list_PayPayhurima,list_kakakucom=list_kakakucom,logs=logs)
+    return render_template('result.html',word=word,list_amazon=list_amazon,list_rakutenn=list_rakutenn,list_PayPayhurima=list_PayPayhurima,list_kakakucom=list_kakakucom)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
